e1c4e46c4eab3b5dca1ec905e18975f667f6ccc5
lls-98 <llsalinas977@gmail.com>
2026-05-14 15:14:40 -0300

    Vincular sw.js a scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,9 @@ window.onload = function() {
   console.log("El archivo JS se cargó correctamente.");
   alert("Archivo JS cargado correctamente.");
 }
+
+if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./sw.js')
+        .then(reg => console.log('Registro de SW exitoso', reg))
+        .catch(err => console.warn('Error al registrar el SW', err));
+}
